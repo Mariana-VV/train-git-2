924b2c5c027585ced25de41da7550f834d3c79b0
Mariana Trinko <marjanavv@gmail.com>
2025-08-27 16:10:51 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://mariana-vv.github.io/train-git-2/",
+});
